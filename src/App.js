473df597b600533b952f312598ab5a5346cf5719
Andres Atehortua <atehortu9620@gmail.com>
2021-12-13 23:04:56 -0500

    making homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './Styles/App.css';
 import log from './Images/Subtract.png';
+import text_1 from './Images/Vector-1.svg';
+import text from './Images/Vector.svg';
 import Home from './Components/HomeImg'
 
 function App() {
@@ -7,6 +9,10 @@ function App() {
     <div className="App">
       <div class="left">
         <img src={log} alt="logo" ></img>
+        <div className="paroles">
+          <img src={text_1} alt="paroles"></img>
+          <img src={text} alt="paroles"></img>
+        </div>
       </div>
   
     </div>
