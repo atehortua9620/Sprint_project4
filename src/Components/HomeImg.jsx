e2a6ea840767752366fece0110d42387c385cbbd
Andres Atehortua <atehortu9620@gmail.com>
2021-12-13 22:41:38 -0500

    starting final sprint projecto

---
+++ src/Components/HomeImg.jsx
@@ -0,0 +1,12 @@
+/* import logo_home from '../Images/logoBG.svg'; */
+
+import '../Styles/Home.css'
+
+export default function Home (){
+
+    return(
+        <div >
+            {/* <img src={logo_home} alt="logo"></img> */}
+        </div>
+    )
+}
